8b8b79822135c84ffef4de5d3fd7d7fc2a69c821
Jorge Medina Rincon <jorgeandresmedinarincon14@gmail.com>
2025-06-01 20:20:46 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-  const cabecera = document.querySelector('.cabecera');
-  if (window.scrollY > 50) {
-    cabecera.classList.add('scrolled');
-  } else {
-    cabecera.classList.remove('scrolled');
-  }
-});
